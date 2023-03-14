17fdb111a4c593f32edbae5e6bed3ad71c4ba73e
Mr. Hood <adamhood15@gmail.com>
2023-03-14 09:21:18 -0500

    Refactored the start function

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,17 @@ var body = document.body;
 //creating question and buttons for quiz
 var question = document.createElement('h2');
 
-var answerChoiceA = document.getElementById('a');
-var answerChoiceB = document.getElementById('b');
-var answerChoiceC = document.getElementById('c');
-var answerChoiceD = document.getElementById('d');
+var buttonA = document.getElementById('a');
+var buttonB = document.getElementById('b');
+var buttonC = document.getElementById('c');
+var buttonD = document.getElementById('d');
+var timer = document.getElementById('time-left');
+var startButton = document.getElementById('start');
+var questionHeader = document.getElementsByClassName('header');
 
+var time = 60;
+
+//Stores all question in object
 var questions = {
     'question1': 'What does HTML stand for?',
     'question2': 'What does CSS stand for?',
@@ -15,6 +21,7 @@ var questions = {
     'question5': 'What does JavaScript do?'
 }
 
+//Store answer objects within an object called answers
 var answers = {
     'answer1' : {'a': 'HyperText Mardown Language', 'b': 'HooperText Markup Language',
                     'b': 'HyperText Markup Language', 'd': 'HighTight Marker Language'},
@@ -33,22 +40,15 @@ var answers = {
                 
 }
 
-var answerA = 'Hyper Toddler Mark Language';
-var answerB = 'Hello Sunny';
-var answerC = 'Butt';
-var answerD = 'Hyper Text Markup Language';
-
-/* When I click start, I want to start the timer 
-and show the first question*/
-var time = 60;
-
-var timer = document.getElementById('time-left');
-var startButton = document.getElementById('start');
-var questionHeader = document.getElementsByClassName('header');
 
 //Initiates the start of the application
 function quizStart() {
 //Timer starts when user clicks start button
+    startTimer();
+
+}
+
+//Timer function
 function startTimer() {
     setInterval (function () {
         if (time > 0) {
@@ -56,21 +56,13 @@ function startTimer() {
         timer.textContent = `Time left: ${time} seconds`;
         } 
     }, 1000);
+}
     
-}
-startTimer();
 
-//Question 1
-document.querySelector('h2').textContent = 'What does HTML stand for?';
-document.querySelector('p').textContent = '';
-document.querySelector('button').style.visibility = 'hidden';
+//Next Question Function
 
-answerChoiceA.style.visibility = 'visible';
-answerChoiceB.style.visibility = 'visible';
-answerChoiceC.style.visibility = 'visible';
-answerChoiceD.style.visibility = 'visible';
 
-}
+
 
 //Create Questions for the test
 
