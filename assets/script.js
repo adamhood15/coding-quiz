22ec3196dcdf1f08413a617769e2b5dbc92a0d23
Mr. Hood <adamhood15@gmail.com>
2023-03-14 14:00:56 -0500

    Added evenListener to all answer choice buttons. Also refactored how the nextquestion function operates

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,14 @@
 var body = document.body;
 //creating question and buttons for quiz
 var questionHeader = document.querySelector('#question-header');
-var buttonA = document.getElementById('a');
-var buttonB = document.getElementById('b');
-var buttonC = document.getElementById('c');
-var buttonD = document.getElementById('d');
+var answerButtonsContainer = document.querySelector('.answer-buttons');
+var answerButtons = document.querySelectorAll('.answer-choices');
 var timer = document.getElementById('time-left');
 var startButton = document.getElementById('start');
-var questionHeader = document.getElementsByClassName('header');
+var answerChoice1 = document.querySelector('#answer-choice-1');
+var answerChoice2 = document.querySelector('#answer-choice-2');
+var answerChoice3 = document.querySelector('#answer-choice-3');
+var answerChoice4 = document.querySelector('#answer-choice-4');
 
 var time = 60;
 
@@ -23,33 +24,29 @@ var questions = {
 //Store answer objects within an object called answers
 var answerChoices = {
     'answer1' : {'a': 'HyperText Mardown Language', 'b': 'HooperText Markup Language',
-                    'b': 'HyperText Markup Language', 'd': 'HighTight Marker Language'},
+                    'c': 'HyperText Markup Language', 'd': 'HighTight Marker Language'},
 
-    'answer2' : {'a': 'HyperText Mardown Language', 'b': 'HooperText Markup Language',
-                    'b': 'HyperText Markup Language', 'd': 'HighTight Marker Language'},
+    'answer2' : {'a': 'Cascading Short Script', 'b': 'Cascading Style Script',
+                    'c': 'Cascading Style Sheet', 'd': 'Cat Stye Shet'},
 
-    'answer3' : {'a': 'HyperText Mardown Language', 'b': 'HooperText Markup Language',
-                    'b': 'HyperText Markup Language', 'd': 'HighTight Marker Language'},
+    'answer3' : {'a': 'Styles the webpage', 'b': 'Contains the content of the webpage',
+                    'c': 'Changes the style of the document body', 'd': 'Makes the webpage interactive'},
 
-    'answer4' : {'a': 'HyperText Mardown Language', 'b': 'HooperText Markup Language',
-                    'b': 'HyperText Markup Language', 'd': 'HighTight Marker Language'},
+    'answer4' : {'a': 'Styles the paragraphs of a webpage', 'b': 'Holds the content of the page',
+                    'c': 'Makes the webpage interactive', 'd': 'Styles the webpage'},
 
-    'answer' : {'a': 'HyperText Mardown Language', 'b': 'HooperText Markup Language',
-                    'b': 'HyperText Markup Language', 'd': 'HighTight Marker Language'}
+    'answer5' : {'a': 'Makes the webpage interactive', 'b': 'Holds the content of the page',
+                    'c': 'Changes the font of the page', 'd': 'Styles the webpage'}
                 
 }
 
 
 //Initiates the start of the application
 function quizStart() {
-//Timer starts when user clicks start button
+  
     startTimer();
-    //clears out starting code for the questions
-    displayChoices();
-
-    nextQuestion();
-
-
+    hideHome();
+    displayQuestion();
 }
 
 //Timer function
@@ -63,39 +60,61 @@ function startTimer() {
 }
 
 
-function displayChoices() {
+function hideHome() {
     document.querySelector('p').style.visibility = 'hidden';
-    document.querySelector('#start').style.visibility = 'hidden';
-    document.querySelector('#a').style.visibility = 'visible';
-    document.querySelector('#b').style.visibility = 'visible';
-    document.querySelector('#c').style.visibility = 'visible';
-    document.querySelector('#d').style.visibility = 'visible';
-
+    startButton.style.visibility = 'hidden';
+   
 }
 
 function hideChoices() {
-    document.querySelector('#a').style.visibility = 'hidden';
-    document.querySelector('#b').style.visibility = 'hidden';
-    document.querySelector('#c').style.visibility = 'hidden';
-    document.querySelector('#d').style.visibility = 'hidden';
+    answerButtonsContainer.style.visibility = 'hidden';
+
 }
 
-console.log(Object.keys(questions).length);
 
-//Next Question Function
-function nextQuestion() {
-    
-    //Object.keys allows you to get the length of the keys of your object
-    //Object.values allows you to pull the values from your object
+function displayQuestion() {
+    document.body.style.backgroundColor = 'blue';
+    answerButtonsContainer.style.visibility = 'visible';
+
+
+    answerChoice1.textContent = Object.values(answerChoices.answer1)[0];
+    answerChoice2.textContent = Object.values(answerChoices.answer1)[1];
+    answerChoice3.textContent = Object.values(answerChoices.answer1)[2];
+    answerChoice4.textContent = Object.values(answerChoices.answer1)[3];
+
     for (var i = 0; i < Object.keys(questions).length; i++) {
-        document.querySelector('h2').textContent = Object.values(questions)[i];
+        var question = questionHeader.textContent = Object.values(questions)[i];
+        return question;
     }
 
 }
 
+//Next Question Function
+function nextQuestion() {
+    body.backgroundColor = 'yellow';
+
+    //Object.keys allows you to get the length of the keys of your object
+    //Object.values allows you to pull the values from your object
+
+        for (var i = 0; i < Object.keys(questions).length; i++) {
+            var question = document.querySelector('h2').textContent = Object.values(questions)[i];
+            return question;
+        }
+
+
+}
+
+
+console.log(document.querySelectorAll('.answer-choices'));
 
 
 //Create Questions for the test
 
 //Add event listeners for each button
 startButton.addEventListener('click', quizStart);
+
+answerButtons.forEach(element => {
+    element.addEventListener('click', function(){
+        questionHeader.style.color = 'blue';
+    })
+}) 
