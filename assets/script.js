b251d770da70603229b78a2ae1345cae256180da
Mr. Hood <adamhood15@gmail.com>
2023-03-16 15:08:26 -0500

    Refactored the code from the top, ended on nextQuestion()

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,7 @@
-var body = document.body;
-//creating question and buttons for quiz
+//References for elements on the webpage
 var questionHeader = document.querySelector('#question-header');
 var answerButtonsContainer = document.querySelector('.answer-buttons');
 var answerButtons = document.querySelectorAll('.answer-choices');
-var timer = document.getElementById('time-left');
-var startButton = document.getElementById('start');
 var answerChoice1 = document.querySelector('#answer-choice-1');
 var answerChoice2 = document.querySelector('#answer-choice-2');
 var answerChoice3 = document.querySelector('#answer-choice-3');
@@ -13,14 +10,16 @@ var nextButton = document.querySelector('#next-question');
 var scoreDisplay = document.querySelector('#score');
 var contentContainer = document.querySelector('.content');
 var tryAgainButton = document.querySelector('#try-again');
+var timer = document.getElementById('time-left');
+var startButton = document.getElementById('start');
 
-var time = 60;
 //counter for next question loop
+var time = 60;
 var counter = 0;
 var score = 0;
 
 
-//Stores all question in array with the answers
+//Stores each question as objects in array
 var questions = [
     {
     'question': 'What does HTML stand for?',
@@ -28,15 +27,17 @@ var questions = [
                 'HooperText Markup Language',
                 'HyperText Markup Language', 
                 'HighTight Marker Language'],
-    'answer': 'HyperText Markdown Language'},
-
+    'answer': 'HyperText Markdown Language'
+    },
+    
     {
     'question': 'What does CSS stand for?',
     'choices': ['Cascading Short Script',
                 'Cascading Style Script', 
                 'Cascading Style Sheet', 
                 'Cat Style Sheet'],
-    'answer': 'Cascading Style Sheet'},
+    'answer': 'Cascading Style Sheet'
+    },
 
     {
     'question': 'What does HTML do?',
@@ -44,87 +45,73 @@ var questions = [
                 'Creates the content of the webpage', 
                 'Changes the style of the document body', 
                 'Sends the browser data to a server'],
-    'answer': 'Creates the content of the webpage'},
+    'answer': 'Creates the content of the webpage'
+    },
+
     {
     'question': 'What does CSS do?',
     'choices': ['Styles the paragraphs of a webpage',
                 'Holds the content of the webpage', 
                 'Contains the content of a webpage', 
                 'Styles the webpage'],
-    'answer': 'Styles the webpage'},
+    'answer': 'Styles the webpage'
+    },
+
     {
     'question': 'What does JavaScript do?',
     'choices': ['Makes the webpage interactive',
                 'Holds the content of the page', 
                 'Changes the font of the page', 
                 'Styles the look of the webpage'],
-    'answer': 'Makes the webpage interactive'}
+    'answer': 'Makes the webpage interactive'
+    }
 ]
 
-const users = [];
 
 //Initiates the start of the application
 function quizStart() {
-  
 
+    //Removes homepage content
     document.body.style.backgroundColor = 'blue';
-    nextButton.style.visibility = 'visible';
+    startButton.style.visibility = 'hidden';
+    document.querySelector('p').remove();
 
-    timeLeft;
-    hideHome();
+
+    //Starts Timer and Displays first question
+    startTimer();
     nextQuestion();
 
 }
 
-//Timer function
-var timeLeft = function startTimer() {
-    setInterval (function () {
-        if (time > 0) {
+//Starts Timer function
+function startTimer() {
+    var startTimer = setInterval (function () {
         time--;
         timer.textContent = `Time left: ${time} seconds`;
-        } else {
+
+        if (time <= 0) {
+            clearInterval(startTimer);
+            endQuiz();
             timer.textContent = `TIME'S UP!`;
-            document.body.style.backgroundColor = 'red';
         }
     }, 1000);
 
-    function stopTimer() {
-        clearInterval(timeLeft);
-    }
 }
-
-function displayScore() {
-    scoreDisplay.textContent = `Score: ${score}`;
-}
-
-//Removes home content from webpage
-function hideHome() {
-    document.querySelector('p').remove();
-    startButton.textContent = 'Next Question';
-    startButton.style.visibility = 'hidden';
-}
-
-//Hide answer choice buttons on page load
-function hideChoices() {
-    answerButtonsContainer.style.visibility = 'hidden';
-
-}
-
 
 //Loops through to the next question every time a user clicks on an answer choice
 function nextQuestion() {
     answerButtonsContainer.style.visibility = 'visible';
     document.body.style.backgroundColor = 'blue';
 
-    endQuiz();
-
-
     for (let i = 0; i <= counter; i++) {
+
             questionHeader.textContent = questions[i].question;
             answerChoice1.textContent = questions[i].choices[0];
             answerChoice2.textContent = questions[i].choices[1];
             answerChoice3.textContent = questions[i].choices[2];
             answerChoice4.textContent = questions[i].choices[3];
+
+       
         }
     
     counter++;
@@ -135,8 +122,6 @@ function nextQuestion() {
 
 function feedback(event) {
 
-
-
     for (let j = 0; j <= questions.length; j++) {
 
         if ((event.target.textContent == questions[0].answer) ||
@@ -146,7 +131,7 @@ function feedback(event) {
             (event.target.textContent == questions[4].answer)) {
 
             score +=5;
-            displayScore();
+            scoreDisplay.textContent = `Score: ${score}`;
             nextQuestion();
             document.body.style.backgroundColor = 'green';
             return;
@@ -155,7 +140,7 @@ function feedback(event) {
             document.body.style.backgroundColor = 'red';
             time -= 5;
             score -= 5;
-            displayScore();
+            scoreDisplay.textContent = `Score: ${score}`;
             return;
         }
     }
@@ -166,7 +151,6 @@ function feedback(event) {
 function endQuiz() {
     answerButtonsContainer.remove();
     questionHeader.remove();
-    nextButton.remove();
     tryAgainButton.style.visibility = 'visible';
     document.body.style.backgroundColor = 'yellow';
 
@@ -218,7 +202,6 @@ function leaderBoardDisplay(event) {
         var userInitials = localStorage.getItem('Initials');
         var userScore = localStorage.getItem('Score');
 
-        console.log(users);
         form.remove();
 
         //Removes submission form
@@ -247,10 +230,8 @@ startButton.addEventListener('click', quizStart);
 answerButtons.forEach(element => {
     element.addEventListener('click', feedback)});
 
-nextButton.addEventListener('click', nextQuestion);
-
 //When you click submit form, this executes
-contentContainer.addEventListener('click', leaderBoardDisplay);
+contentContainer.addEventListener('dblclick', leaderBoardDisplay);
 
 tryAgainButton.addEventListener('click', function() {
     window.location.reload();
